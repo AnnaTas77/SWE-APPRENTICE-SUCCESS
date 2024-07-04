afa8950aa7d19460ef3bce3bccff4097049582f1
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-04 15:57:45 +0100

    Completed task 9 using TypeScript. Passed all tests.

--- 01/1-primers/9-scope/index.ts
+++ 01/1-primers/9-scope/index.ts
@@ -1,8 +1,11 @@
 // 1. Currently there is an error in the scope of fixMyScope. Fix the code so that it can be exported to our tests.
-var fixMyScope;
+
+let fixMyScope:string;
+
 {
   fixMyScope = "Help export me";
 }
+
 // 2. A function called updateCity has been written to update the variable myCity. Unfortunately, it currently is returning undefined when the function is executed. Fix scope so that myCity is updated with the specified city everytime updateCity is run.
 /*
 Examples
@@ -10,11 +13,14 @@ updateCity("New York"); // myCity now has the value "New York"
 updateCity("London"); // myCity now has the value "London"
 updateCity("Chicago");
 */
-var myCity;
-function updateCity(city) {
+
+let myCity:string;
+
+function updateCity(city:string):string {
   myCity = city;
   return myCity;
 }
+
 // 3. Create a global variable called currentSum that contains an initial value of zero. Next, create a function called `incrementSum` that increments the currentSum by 1 every time that it is run.
 /*
 Example
@@ -23,11 +29,14 @@ incrementSum()
 incrementSum()
 console.log(currentSum) // Outputs the value 3
 */
-var currentSum = 0;
-function incrementSum() {
+
+let currentSum:number = 0;
+
+function incrementSum():number {
   return (currentSum += 1);
 }
+
 // DO NOT EDIT BELOW
 console.log(updateCity("New York"));
 
-export { fixMyScope, myCity, incrementSum };
+
